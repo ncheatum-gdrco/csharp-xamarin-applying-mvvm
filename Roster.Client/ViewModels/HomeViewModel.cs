9c4783708e93173deedd56cb0d3c608bc4fa9940
Neil Cheatum <NCheatum@greendiamond.com>
2021-01-26 11:45:54 -0800

    Add behavior to viewmodel

--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Xamarin.Forms;
 
@@ -9,11 +10,31 @@ namespace Roster.Client.ViewModels
     public class HomeViewModel : System.ComponentModel.INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        public string Title { get; set; } = "Roster App";
+        private string title = "Roster App";
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (value != title)
+                {
+                    title = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
 
         public Command UpdateApplicationCommand { get; set; }
 
+        public HomeViewModel()
+        {
+            UpdateApplicationCommand = new Command(execute: () => this.Title = "Roster App (v2.0)");
+        }
 
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
 
     }
 }
