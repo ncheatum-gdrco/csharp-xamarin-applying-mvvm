331d6a3e3fd33d009c2d3ad0453d2b2e225c3e2b
Neil Cheatum <NCheatum@greendiamond.com>
2021-01-26 12:06:56 -0800

    create model class

---
+++ Roster.Client/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Roster.Client.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Company { get; set; }
+    }
+}
--- Roster.Client/ViewModels/HomeViewModel.cs
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Roster.Client.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -24,11 +26,20 @@ namespace Roster.Client.ViewModels
             }
         }
 
+        public ObservableCollection<Person> People { get; private set; }
+
         public Command UpdateApplicationCommand { get; set; }
 
         public HomeViewModel()
         {
             UpdateApplicationCommand = new Command(execute: () => this.Title = "Roster App (v2.0)");
+
+            People = new ObservableCollection<Person>
+            {
+                new Person { Name = "Delores Feil", Company = "Legros Group"},
+                new Person { Name = "Ann Zboncak", Company = "Ledner - Ferry" },
+                new Person { Name = "Jaime Lesch", Company = "Herzog and Sons" }
+            };
         }
 
         private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
